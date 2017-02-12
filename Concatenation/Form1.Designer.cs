a60e0b0c388acab9ba727998729ff520b9d6e708
jcsme <jcsme@DESKTOP-O6725QL>
2017-02-12 13:07:57 -0500

    Add project files.

---
+++ Concatenation/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace Concatenation
+{
+    partial class Concatenation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnAfternoon = new System.Windows.Forms.Button();
+            this.btnLunch = new System.Windows.Forms.Button();
+            this.btnPlaceYear = new System.Windows.Forms.Button();
+            this.btnBirthPlace = new System.Windows.Forms.Button();
+            this.btnFavorites = new System.Windows.Forms.Button();
+            this.btnPhone = new System.Windows.Forms.Button();
+            this.btnNewLines = new System.Windows.Forms.Button();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.txtFirst = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtLast = new System.Windows.Forms.TextBox();
+            this.txtCountry = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(302, 473);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 25);
+            this.btnExit.TabIndex = 13;
+            this.btnExit.Text = "E&XIT";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(218, 473);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 25);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "CLEA&R";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnAfternoon
+            // 
+            this.btnAfternoon.Location = new System.Drawing.Point(13, 441);
+            this.btnAfternoon.Name = "btnAfternoon";
+            this.btnAfternoon.Size = new System.Drawing.Size(120, 25);
+            this.btnAfternoon.TabIndex = 8;
+            this.btnAfternoon.Text = "Afternoon";
+            this.btnAfternoon.UseVisualStyleBackColor = true;
+            this.btnAfternoon.Click += new System.EventHandler(this.btnAfternoon_Click);
+            // 
+            // btnLunch
+            // 
+            this.btnLunch.Location = new System.Drawing.Point(13, 401);
+            this.btnLunch.Name = "btnLunch";
+            this.btnLunch.Size = new System.Drawing.Size(120, 25);
+            this.btnLunch.TabIndex = 7;
+            this.btnLunch.Text = "Lunch";
+            this.btnLunch.UseVisualStyleBackColor = true;
+            this.btnLunch.Click += new System.EventHandler(this.btnLunch_Click);
+            // 
+            // btnPlaceYear
+            // 
+            this.btnPlaceYear.Location = new System.Drawing.Point(13, 354);
+            this.btnPlaceYear.Name = "btnPlaceYear";
+            this.btnPlaceYear.Size = new System.Drawing.Size(120, 25);
+            this.btnPlaceYear.TabIndex = 6;
+            this.btnPlaceYear.Text = "Birth Place and Year";
+            this.btnPlaceYear.UseVisualStyleBackColor = true;
+            this.btnPlaceYear.Click += new System.EventHandler(this.btnPlaceYear_Click);
+            // 
+            // btnBirthPlace
+            // 
+            this.btnBirthPlace.Location = new System.Drawing.Point(13, 310);
+            this.btnBirthPlace.Name = "btnBirthPlace";
+            this.btnBirthPlace.Size = new System.Drawing.Size(120, 25);
+            this.btnBirthPlace.TabIndex = 5;
+            this.btnBirthPlace.Text = "Birth Place";
+            this.btnBirthPlace.UseVisualStyleBackColor = true;
+            this.btnBirthPlace.Click += new System.EventHandler(this.btnBirthPlace_Click);
+            // 
+            // btnFavorites
+            // 
+            this.btnFavorites.Location = new System.Drawing.Point(217, 310);
+            this.btnFavorites.Name = "btnFavorites";
+            this.btnFavorites.Size = new System.Drawing.Size(120, 25);
+            this.btnFavorites.TabIndex = 9;
+            this.btnFavorites.Text = "My Favorites";
+            this.btnFavorites.UseVisualStyleBackColor = true;
+            this.btnFavorites.Click += new System.EventHandler(this.btnFavorites_Click);
+            // 
+            // btnPhone
+            // 
+            this.btnPhone.Location = new System.Drawing.Point(217, 354);
+            this.btnPhone.Name = "btnPhone";
+            this.btnPhone.Size = new System.Drawing.Size(120, 25);
+            this.btnPhone.TabIndex = 10;
+            this.btnPhone.Text = "Smartphone";
+            this.btnPhone.UseVisualStyleBackColor = true;
+            this.btnPhone.Click += new System.EventHandler(this.btnPhone_Click);
+            // 
+            // btnNewLines
+            // 
+            this.btnNewLines.Location = new System.Drawing.Point(217, 401);
+            this.btnNewLines.Name = "btnNewLines";
+            this.btnNewLines.Size = new System.Drawing.Size(120, 25);
+            this.btnNewLines.TabIndex = 11;
+            this.btnNewLines.Text = "Display New Lines";
+            this.btnNewLines.UseVisualStyleBackColor = true;
+            this.btnNewLines.Click += new System.EventHandler(this.btnNewLines_Click);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(13, 13);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(364, 137);
+            this.txtOutput.TabIndex = 0;
+            // 
+            // txtFirst
+            // 
+            this.txtFirst.Location = new System.Drawing.Point(13, 185);
+            this.txtFirst.Name = "txtFirst";
+            this.txtFirst.Size = new System.Drawing.Size(160, 20);
+            this.txtFirst.TabIndex = 1;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(13, 261);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(160, 20);
+            this.txtCity.TabIndex = 3;
+            // 
+            // txtLast
+            // 
+            this.txtLast.Location = new System.Drawing.Point(217, 184);
+            this.txtLast.Name = "txtLast";
+            this.txtLast.Size = new System.Drawing.Size(160, 20);
+            this.txtLast.TabIndex = 2;
+            // 
+            // txtCountry
+            // 
+            this.txtCountry.Location = new System.Drawing.Point(217, 260);
+            this.txtCountry.Name = "txtCountry";
+            this.txtCountry.Size = new System.Drawing.Size(160, 20);
+            this.txtCountry.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 166);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "&First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(217, 165);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "&Last Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(16, 242);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "&City of Birth";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(217, 242);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "C&ountry of Birth";
+            // 
+            // Concatenation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 504);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCountry);
+            this.Controls.Add(this.txtLast);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtFirst);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnNewLines);
+            this.Controls.Add(this.btnPhone);
+            this.Controls.Add(this.btnFavorites);
+            this.Controls.Add(this.btnBirthPlace);
+            this.Controls.Add(this.btnPlaceYear);
+            this.Controls.Add(this.btnLunch);
+            this.Controls.Add(this.btnAfternoon);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnExit);
+            this.Name = "Concatenation";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnAfternoon;
+        private System.Windows.Forms.Button btnLunch;
+        private System.Windows.Forms.Button btnPlaceYear;
+        private System.Windows.Forms.Button btnBirthPlace;
+        private System.Windows.Forms.Button btnFavorites;
+        private System.Windows.Forms.Button btnPhone;
+        private System.Windows.Forms.Button btnNewLines;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.TextBox txtFirst;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtLast;
+        private System.Windows.Forms.TextBox txtCountry;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Concatenation/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Concatenation
+{
+    public partial class Concatenation : Form
+    {
+        public Concatenation()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtOutput.Clear();
+            txtFirst.Clear();
+            txtLast.Clear();
+            txtCity.Clear();
+            txtCountry.Clear();
+        }
+
+        private void btnBirthPlace_Click(object sender, EventArgs e)
+        {
+            txtOutput.Text = txtFirst.Text + " " + txtLast.Text +
+                " was born in " + txtCity.Text + ", " + txtCountry.Text + ".";
+        }
+
+        private void btnPlaceYear_Click(object sender, EventArgs e)
+        {
+            string strYear = Interaction.InputBox(
+                txtFirst.Text + ", what year were you born?");
+            txtOutput.Text = "in " + strYear + ", " + txtFirst.Text +
+                " " + txtLast.Text + " was born in " + txtCity.Text +
+                ", " + txtCountry.Text + ".";
+        }
+
+        private void btnLunch_Click(object sender, EventArgs e)
+        {
+            string strName = Interaction.InputBox("What is your name?");
+            string strLunch = Interaction.InputBox("Hi, " + strName +
+                ", what did you have for lunch?");
+            txtOutput.Text = strName + " had " + strLunch + " for lunch.";
+        }
+
+        private void btnNewLines_Click(object sender, EventArgs e)
+        {
+            txtOutput.Text = ControlChars.Tab + "Store Hours" +
+                Environment.NewLine + Environment.NewLine +
+                "Monday to Friday: 10:00am to 10:00pm" + Environment.NewLine +
+                "Saturday: 12:00pm to 19:00pm" + Environment.NewLine +
+                "Sunday: Closed"; 
+        }
+
+        private void btnFavorites_Click(object sender, EventArgs e)
+        {
+            string strName = Interaction.InputBox("What is your name?");
+            string strDrink = Interaction.InputBox("What is your favorite drink?");
+            string strFood = Interaction.InputBox("What is your favorite food?");
+            string strMovie = Interaction.InputBox("What is your favorite movie?");
+            txtOutput.Text = strName + " enjoys watching " + strMovie + " while drinking " +
+                strDrink + " and eating " + strFood + "."; 
+        }
+
+        private void btnAfternoon_Click(object sender, EventArgs e)
+        {
+            string strName = Interaction.InputBox("What is your name?");
+            string strPlace = Interaction.InputBox("Hi " + strName +
+                ". where were you this afternoon?");
+            txtOutput.Text = strName + " was at/in " + strPlace + " this afternoon.";
+        }
+
+        private void btnPhone_Click(object sender, EventArgs e)
+        {
+            string strModel = Interaction.InputBox("What type of phone do you have?");
+            string strPrice = Interaction.InputBox("How much did you pay for it?");
+            txtOutput.Text = "You have paid $" + strPrice + " for a " + strModel + ".";
+        }
+    }
+}
